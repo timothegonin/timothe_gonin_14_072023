9acd906ca7dcd0afdf07a689c974e7c13830fa2c
TimotheGonin <gonin.timothe@gmail.com>
2023-07-15 13:25:31 +0200

    🚧 import js in component

--- src/features/createEmployee/CreateEmployeeView.jsx
+++ src/features/createEmployee/CreateEmployeeView.jsx
@@ -1,50 +1,381 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CreateEmployeeView = () => {
+  // State for form fields
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dateOfBirth, setDateOfBirth] = useState('')
+  const [startDate, setStartDate] = useState('')
+  const [department, setDepartment] = useState('Sales')
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [zipCode, setZipCode] = useState('')
+
+  // Load states options on component mount
+  useEffect(() => {
+    const stateSelect = document.getElementById('state')
+    states.forEach((state) => {
+      const option = document.createElement('option')
+      option.value = state.abbreviation
+      option.text = state.name
+      stateSelect.appendChild(option)
+    })
+  }, [])
+
+  const handleSaveEmployee = (e) => {
+    e.preventDefault()
+    const employees = JSON.parse(localStorage.getItem('employees')) || []
+    const employee = {
+      firstName,
+      lastName,
+      dateOfBirth,
+      startDate,
+      department,
+      street,
+      city,
+      state,
+      zipCode,
+    }
+    employees.push(employee)
+    localStorage.setItem('employees', JSON.stringify(employees))
+  }
+
   return (
     <section className="container">
       <h2>CreateEmployee</h2>
-      <form action="#" id="create-employee">
-        <label for="first-name">First Name</label>
-        <input type="text" id="first-name" />
+      <form action="#" id="create-employee" onSubmit={handleSaveEmployee}>
+        <label htmlFor="first-name">First Name</label>
+        <input
+          type="text"
+          id="first-name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
 
-        <label for="last-name">Last Name</label>
-        <input type="text" id="last-name" />
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          type="text"
+          id="last-name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
 
-        <label for="date-of-birth">Date of Birth</label>
-        <input id="date-of-birth" type="text" />
+        <label htmlFor="date-of-birth">Date of Birth</label>
+        <input
+          id="date-of-birth"
+          type="text"
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
 
-        <label for="start-date">Start Date</label>
-        <input id="start-date" type="text" />
+        <label htmlFor="start-date">Start Date</label>
+        <input
+          id="start-date"
+          type="text"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
 
-        <fieldset class="address">
+        <fieldset className="address">
           <legend>Address</legend>
 
-          <label for="street">Street</label>
-          <input id="street" type="text" />
+          <label htmlFor="street">Street</label>
+          <input
+            id="street"
+            type="text"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+          />
 
-          <label for="city">City</label>
-          <input id="city" type="text" />
+          <label htmlFor="city">City</label>
+          <input
+            id="city"
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
 
-          <label for="state">State</label>
-          <select name="state" id="state"></select>
+          <label htmlFor="state">State</label>
+          <select
+            name="state"
+            id="state"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+          >
+            {states.map((state) => (
+              <option key={state.abbreviation} value={state.abbreviation}>
+                {state.name}
+              </option>
+            ))}
+          </select>
 
-          <label for="zip-code">Zip Code</label>
-          <input id="zip-code" type="number" />
+          <label htmlFor="zip-code">Zip Code</label>
+          <input
+            id="zip-code"
+            type="number"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+          />
         </fieldset>
 
-        <label for="department">Department</label>
-        <select name="department" id="department">
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
+        <label htmlFor="department">Department</label>
+        <select
+          name="department"
+          id="department"
+          value={department}
+          onChange={(e) => setDepartment(e.target.value)}
+        >
+          <option value="Sales">Sales</option>
+          <option value="Marketing">Marketing</option>
+          <option value="Engineering">Engineering</option>
+          <option value="Human Resources">Human Resources</option>
+          <option value="Legal">Legal</option>
         </select>
+
+        <button type="submit">Save</button>
       </form>
-      <button>Save</button>
     </section>
   )
 }
 
 export default CreateEmployeeView
+
+const states = [
+  {
+    name: 'Alabama',
+    abbreviation: 'AL',
+  },
+  {
+    name: 'Alaska',
+    abbreviation: 'AK',
+  },
+  {
+    name: 'American Samoa',
+    abbreviation: 'AS',
+  },
+  {
+    name: 'Arizona',
+    abbreviation: 'AZ',
+  },
+  {
+    name: 'Arkansas',
+    abbreviation: 'AR',
+  },
+  {
+    name: 'California',
+    abbreviation: 'CA',
+  },
+  {
+    name: 'Colorado',
+    abbreviation: 'CO',
+  },
+  {
+    name: 'Connecticut',
+    abbreviation: 'CT',
+  },
+  {
+    name: 'Delaware',
+    abbreviation: 'DE',
+  },
+  {
+    name: 'District Of Columbia',
+    abbreviation: 'DC',
+  },
+  {
+    name: 'Federated States Of Micronesia',
+    abbreviation: 'FM',
+  },
+  {
+    name: 'Florida',
+    abbreviation: 'FL',
+  },
+  {
+    name: 'Georgia',
+    abbreviation: 'GA',
+  },
+  {
+    name: 'Guam',
+    abbreviation: 'GU',
+  },
+  {
+    name: 'Hawaii',
+    abbreviation: 'HI',
+  },
+  {
+    name: 'Idaho',
+    abbreviation: 'ID',
+  },
+  {
+    name: 'Illinois',
+    abbreviation: 'IL',
+  },
+  {
+    name: 'Indiana',
+    abbreviation: 'IN',
+  },
+  {
+    name: 'Iowa',
+    abbreviation: 'IA',
+  },
+  {
+    name: 'Kansas',
+    abbreviation: 'KS',
+  },
+  {
+    name: 'Kentucky',
+    abbreviation: 'KY',
+  },
+  {
+    name: 'Louisiana',
+    abbreviation: 'LA',
+  },
+  {
+    name: 'Maine',
+    abbreviation: 'ME',
+  },
+  {
+    name: 'Marshall Islands',
+    abbreviation: 'MH',
+  },
+  {
+    name: 'Maryland',
+    abbreviation: 'MD',
+  },
+  {
+    name: 'Massachusetts',
+    abbreviation: 'MA',
+  },
+  {
+    name: 'Michigan',
+    abbreviation: 'MI',
+  },
+  {
+    name: 'Minnesota',
+    abbreviation: 'MN',
+  },
+  {
+    name: 'Mississippi',
+    abbreviation: 'MS',
+  },
+  {
+    name: 'Missouri',
+    abbreviation: 'MO',
+  },
+  {
+    name: 'Montana',
+    abbreviation: 'MT',
+  },
+  {
+    name: 'Nebraska',
+    abbreviation: 'NE',
+  },
+  {
+    name: 'Nevada',
+    abbreviation: 'NV',
+  },
+  {
+    name: 'New Hampshire',
+    abbreviation: 'NH',
+  },
+  {
+    name: 'New Jersey',
+    abbreviation: 'NJ',
+  },
+  {
+    name: 'New Mexico',
+    abbreviation: 'NM',
+  },
+  {
+    name: 'New York',
+    abbreviation: 'NY',
+  },
+  {
+    name: 'North Carolina',
+    abbreviation: 'NC',
+  },
+  {
+    name: 'North Dakota',
+    abbreviation: 'ND',
+  },
+  {
+    name: 'Northern Mariana Islands',
+    abbreviation: 'MP',
+  },
+  {
+    name: 'Ohio',
+    abbreviation: 'OH',
+  },
+  {
+    name: 'Oklahoma',
+    abbreviation: 'OK',
+  },
+  {
+    name: 'Oregon',
+    abbreviation: 'OR',
+  },
+  {
+    name: 'Palau',
+    abbreviation: 'PW',
+  },
+  {
+    name: 'Pennsylvania',
+    abbreviation: 'PA',
+  },
+  {
+    name: 'Puerto Rico',
+    abbreviation: 'PR',
+  },
+  {
+    name: 'Rhode Island',
+    abbreviation: 'RI',
+  },
+  {
+    name: 'South Carolina',
+    abbreviation: 'SC',
+  },
+  {
+    name: 'South Dakota',
+    abbreviation: 'SD',
+  },
+  {
+    name: 'Tennessee',
+    abbreviation: 'TN',
+  },
+  {
+    name: 'Texas',
+    abbreviation: 'TX',
+  },
+  {
+    name: 'Utah',
+    abbreviation: 'UT',
+  },
+  {
+    name: 'Vermont',
+    abbreviation: 'VT',
+  },
+  {
+    name: 'Virgin Islands',
+    abbreviation: 'VI',
+  },
+  {
+    name: 'Virginia',
+    abbreviation: 'VA',
+  },
+  {
+    name: 'Washington',
+    abbreviation: 'WA',
+  },
+  {
+    name: 'West Virginia',
+    abbreviation: 'WV',
+  },
+  {
+    name: 'Wisconsin',
+    abbreviation: 'WI',
+  },
+  {
+    name: 'Wyoming',
+    abbreviation: 'WY',
+  },
+]
