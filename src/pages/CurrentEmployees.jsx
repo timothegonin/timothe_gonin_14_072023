adeeb7878edf14e4dc128eb67745b5cf84ab84ed
TimotheGonin <gonin.timothe@gmail.com>
2023-07-25 16:28:44 +0200

    🚧 employees table structure

--- src/pages/CurrentEmployees.jsx
+++ src/pages/CurrentEmployees.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CurrentEmployees = () => {
+  const [employees, setEmployees] = useState([])
+
+  useEffect(() => {
+    const storedEmployees = JSON.parse(localStorage.getItem('employees')) || []
+    setEmployees(storedEmployees)
+  }, [])
+
   return (
-    <div>CurrentEmployees page</div>
+    <div id="employee-div" className="container">
+      <h1>Current Employees</h1>
+      <table className="employee-table display">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Start Date</th>
+            <th>Department</th>
+            <th>Date of Birth</th>
+            <th>Street</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee, index) => (
+            <tr key={index}>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.startDate}</td>
+              <td>{employee.department}</td>
+              <td>{employee.dateOfBirth}</td>
+              <td>{employee.street}</td>
+              <td>{employee.city}</td>
+              <td>{employee.state}</td>
+              <td>{employee.zipCode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
