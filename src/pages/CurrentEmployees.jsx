118125c004f4dd3296c40c0929de249a31cf7978
TimotheGonin <gonin.timothe@gmail.com>
2023-07-25 10:01:37 +0200

    ✨ employees route

---
+++ src/pages/CurrentEmployees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CurrentEmployees = () => {
+  return (
+    <div>CurrentEmployees page</div>
+  )
+}
+
+export default CurrentEmployees
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import CreateEmployee from '../pages/CreateEmployee'
+import CurrentEmployees from '../pages/CurrentEmployees'
 
 /**
  * Router component responsible for handling the application's routing.
@@ -10,6 +11,7 @@ const Router = () => {
   return (
     <Routes>
       <Route exact path="/" element={<CreateEmployee />} />
+      <Route path="/employees" element={<CurrentEmployees />} />
       <Route path="*" element={<p>Error</p>} />
     </Routes>
   )
