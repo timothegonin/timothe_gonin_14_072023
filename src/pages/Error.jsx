b48fcd6452d080896b68e93969ee58408c281b4d
TimotheGonin <gonin.timothe@gmail.com>
2023-07-31 11:30:48 +0200

    🚸 error page links

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import Button from 'react-bootstrap/Button'
 
 const Error = () => {
@@ -7,12 +8,17 @@ const Error = () => {
       <h2>Erreur</h2>
       <span>Redirection timer ...</span>
       <div className="d-grid gap-2">
-        <Button variant="outline-primary" size="lg">
-          Home
-        </Button>
-        <Button variant="outline-primary" size="lg">
-          Employees list
-        </Button>
+        <LinkContainer to="/">
+          <Button variant="outline-primary" size="lg">
+            Home
+          </Button>
+        </LinkContainer>
+
+        <LinkContainer to="/employees">
+          <Button variant="outline-primary" size="lg">
+            Employees list
+          </Button>
+        </LinkContainer>
       </div>
     </main>
   )
