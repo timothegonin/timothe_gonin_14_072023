350e415331af3650553dfcbcd3faca2d30a70663
TimotheGonin <gonin.timothe@gmail.com>
2023-07-31 15:30:27 +0200

    🚧 redirection + displaying timer

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import Button from 'react-bootstrap/Button'
 
 const Error = () => {
+  const navigate = useNavigate()
+  const [countdown, setCountdown] = useState(5)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1)
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate('/')
+    }
+  }, [countdown, navigate])
+
   return (
     <main className="wrapper">
       <h2>Erreur</h2>
-      <span>Redirection timer ...</span>
+      <span>Redirection timer ... {countdown}</span>
       <div className="d-grid gap-2">
         <LinkContainer to="/">
           <Button variant="outline-primary" size="lg">
