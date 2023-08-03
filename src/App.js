b39d7b916e80097d04f6b9d6be4b6915f3287857
TimotheGonin <gonin.timothe@gmail.com>
2023-08-03 09:40:50 +0200

    💩 modal component, need to be improved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Router from './router/Router'
 
+import Button from 'react-bootstrap/Button'
+import MyVerticallyCenteredModal from './components/Modal'
+
 function App() {
+  const [modalShow, setModalShow] = useState(false)
   return (
     <React.Fragment>
       <Header />
       <Router />
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </React.Fragment>
   )
 }
