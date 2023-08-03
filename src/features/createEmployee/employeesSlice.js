92090463949cacf1fff0ae8e854f9354a164786d
TimotheGonin <gonin.timothe@gmail.com>
2023-08-03 15:36:02 +0200

    ♻️ 🚧 modal actions to the employeesSlice

--- src/features/createEmployee/employeesSlice.js
+++ src/features/createEmployee/employeesSlice.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
   employees: JSON.parse(localStorage.getItem('employees')) || [],
+  showConfirmationModal: false,
 }
 
 const employeesSlice = createSlice({
@@ -17,8 +18,15 @@ const employeesSlice = createSlice({
       state.employees.push(newEmployee)
       localStorage.setItem('employees', JSON.stringify(state.employees))
     },
+    showConfirmationModal: (state) => {
+      state.showConfirmationModal = true
+    },
+    hideConfirmationModal: (state) => {
+      state.showConfirmationModal = false
+    },
   },
 })
 
-export const { createEmployee } = employeesSlice.actions
+export const { createEmployee, showConfirmationModal, hideConfirmationModal } =
+  employeesSlice.actions
 export default employeesSlice.reducer
