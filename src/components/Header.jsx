d6eeadf325941f9eda11005d89031bff05aa9f47
TimotheGonin <gonin.timothe@gmail.com>
2023-09-19 17:03:39 +0200

    📝 components documentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,15 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
+/**
+ * The `Header` component represents the header navigation bar of the application.
+ * It includes a navigation brand logo and a link to toggle between the home page and the employee list page.
+ *
+ * @returns {JSX.Element} The rendered Header component.
+ */
 const Header = () => {
   const { pathname } = useLocation()
+
   return (
     <header>
       <Navbar bg="light" data-bs-theme="light">
