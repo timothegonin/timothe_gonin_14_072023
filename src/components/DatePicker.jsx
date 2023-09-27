f251ef9b2ddf035b7e841594fa68d8084dee49a5
TimotheGonin <gonin.timothe@gmail.com>
2023-09-27 14:41:46 +0200

    🚸 📝 add portal function + update doc

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -6,47 +6,31 @@ import Form from 'react-bootstrap/Form'
 import { format } from 'date-fns'
 
 /**
- * The `CustomDatePicker` component is a custom date picker component built on top of
- * the `react-datepicker` library. It provides a styled input field for selecting dates.
+ * The `CustomDatePicker` component represents a customized date picker input field.
  *
  * @component
- * @param {string} label - The label text for the date picker input.
- * @param {string} htmlForLabel - The `htmlFor` attribute for the associated label.
- * @param {string} value - The initial date value in "MM-dd-yyyy" format (e.g., "09-18-2023").
- * @param {function} handler - A callback function that is called when a date is selected.
- *   It receives the selected date in "MM-dd-yyyy" format as an argument.
- * @returns {JSX.Element} The rendered CustomDatePicker component.
+ * @param {object} props - The component's properties.
+ * @param {string} props.label - The label for the date picker field.
+ * @param {string} props.htmlForLabel - The ID associated with the date picker field.
+ * @param {string} props.value - The current value of the date picker field.
+ * @param {function} props.handler - The callback function for handling date changes.
+ * @returns {JSX.Element} - The rendered CustomDatePicker component.
  */
 const CustomDatePicker = ({ label, htmlForLabel, value, handler }) => {
-  /**
-   * The `CustomInput` component is a custom input field for the date picker.
-   * It renders a Form.Group containing a Form.Label, Form.Control input, and validation feedback.
-   *
-   * @param {object} props - The component's props.
-   * @param {string} props.value - The input value (date).
-   * @param {function} props.onClick - The click event handler.
-   * @param {React.Ref} ref - A ref forwarded by the date picker.
-   * @returns {JSX.Element} The rendered CustomInput component.
-   */
   const CustomInput = forwardRef(({ value, onClick }, ref) => (
-    <Form.Group>
-      <Form.Label htmlFor={htmlForLabel}>{label}</Form.Label>
-      <Form.Control
-        required
-        type="text"
-        defaultValue={value}
-        onClick={onClick}
-        ref={ref}
-        placeholder="month/day/year"
-      />
-      <Form.Control.Feedback type="invalid">
-        Please choose a {label.toLowerCase()}.
-      </Form.Control.Feedback>
-    </Form.Group>
+    <Form.Control
+      required
+      type="text"
+      defaultValue={value}
+      onClick={onClick}
+      ref={ref}
+      placeholder="month/day/year"
+    />
   ))
 
   return (
-    <React.Fragment>
+    <Form.Group>
+      <Form.Label htmlFor={htmlForLabel}>{label}</Form.Label>
       <DatePicker
         selected={value ? new Date(value) : value}
         onChange={(date) => handler(format(date, 'MM-dd-yyyy'))}
@@ -55,8 +39,12 @@ const CustomDatePicker = ({ label, htmlForLabel, value, handler }) => {
         aria-label={`${label} input`}
         wrapperClassName="d-block"
         customInput={<CustomInput />}
+        withPortal
       />
-    </React.Fragment>
+      <Form.Control.Feedback type="invalid">
+        Please choose a {label.toLowerCase()}.
+      </Form.Control.Feedback>
+    </Form.Group>
   )
 }
 
